950a33c49999f74bd0770c04ec420b475117d3e0
Biruk Tebebe <brook.diku1@gmail.com>
2022-11-27 17:51:51 +0300

    Initial Commit
    Starter code for the project with next js and node js

---
+++ client/pages/events/event/[eventId].js
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+const Event = () => {
+  const router = useRouter();
+  return <h1>{router.query.eventId}</h1>;
+};
+export default Event;
+ 
---
+++ client/pages/index.js
@@ -0,0 +1,4 @@
+function Index() {
+ return <h1>Home</h1> 
+};
+export default Index;
